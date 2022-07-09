270aefb6861d2351b7a9a20b5e4d5499128fb28a
Nick DeRobertis <whoopnip@gmail.com>
2022-07-09 18:31:17 -0400

    chore: Remove hamburger menu

--- output/components/layout/NavBar.tsx
+++ output/components/layout/NavBar.tsx
@@ -25,30 +25,8 @@ export default function NavBar(): JSX.Element {
   return (
     <>
       <nav className="navbar navbar-expand-lg shadow-md py-2 bg-white relative flex items-center w-full justify-between">
-        <div className="px-6 w-full flex flex-wrap items-center justify-between">
-          <div className="flex items-center">
-            <button
-              className="navbar-toggler border-0 py-3 lg:hidden leading-none text-xl bg-transparent text-gray-600 hover:text-gray-700 focus:text-gray-700 transition-shadow duration-150 ease-in-out mr-2"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContentY"
-              aria-controls="navbarSupportedContentY"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <svg
-                aria-hidden="true"
-                data-prefix="fas"
-                className="w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
-                ></path>
-              </svg>
-            </button>
+        <div className="pl-2 pr-6 w-full flex flex-wrap items-center justify-between">
+          <div className="flex items-center pr-5">
             <Link className="navbar-brand text-blue-600" href="/">
               <svg
                 className="w-5 h-5 ml-2 lg:ml-0 mr-2"
