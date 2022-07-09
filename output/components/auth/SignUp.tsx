ec44c31f0840d405bb8d0acdde0804b34702b5a8
Nick DeRobertis <whoopnip@gmail.com>
2022-07-09 09:24:14 -0400

    feat: Add a basic structure for home page and sign up

---
+++ output/components/auth/SignUp.tsx
@@ -0,0 +1,57 @@
+export default function SignUp(): JSX.Element {
+  return (
+    <div className="block rounded-lg shadow-lg bg-white px-6 py-12 md:px-12">
+      <form>
+        <div className="grid md:grid-cols-2 md:gap-6">
+          <div className="mb-6">
+            <input
+              type="text"
+              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              placeholder="First name"
+            />
+          </div>
+          <div className="mb-6">
+            <input
+              type="text"
+              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              placeholder="Last name"
+            />
+          </div>
+        </div>
+        <input
+          type="email"
+          className="form-control block w-full px-3 py-1.5 mb-6 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+          placeholder="Email address"
+        />
+        <input
+          type="password"
+          className="form-control block w-full px-3 py-1.5 mb-6 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+          placeholder="Password"
+        />
+        <div className="form-check flex justify-center mb-6">
+          <input
+            className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+            type="checkbox"
+            value=""
+            id="flexCheckChecked"
+            checked
+          />
+          <label
+            className="form-check-label inline-block text-gray-800"
+            htmlFor="flexCheckChecked"
+          >
+            Subscribe to our newsletter
+          </label>
+        </div>
+        <button
+          type="button"
+          data-mdb-ripple="true"
+          data-mdb-ripple-color="light"
+          className="inline-block px-6 py-2.5 mb-6 w-full bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        >
+          Sign up
+        </button>
+      </form>
+    </div>
+  );
+}
--- output/pages/about.tsx
+++ output/pages/about.tsx
@@ -1,8 +1,12 @@
-export default function About() {
+import { NextPage } from "next";
+
+const About: NextPage = () => {
   return (
     <div>
       <h1>About</h1>
       <p>This is the about page</p>
     </div>
   );
-}
+};
+
+export default About;
