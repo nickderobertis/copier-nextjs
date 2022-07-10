78d08a1f48a9bcfebf7dd5d101d17dca3ec4bcc3
Nick DeRobertis <whoopnip@gmail.com>
2022-07-09 20:25:21 -0400

    chore: Restructure forgot password page layout into a common component

--- output/pages/forgot-password.tsx
+++ output/pages/forgot-password.tsx
@@ -1,13 +1,12 @@
 import { NextPage } from "next";
 import ForgotPasswordBegin from "../components/auth/forgot-password/ForgotPasswordBegin";
-import styles from "./forgot-password.module.css";
+import ForgotPasswordPageLayout from "../components/auth/forgot-password/ForgotPasswordPageLayout";
 
 const ForgotPassword: NextPage = () => {
-  const wrapperStyles = `${styles.vh} flex justify-center content-center items-center flex-wrap`;
   return (
-    <div className={wrapperStyles}>
+    <ForgotPasswordPageLayout>
       <ForgotPasswordBegin />
-    </div>
+    </ForgotPasswordPageLayout>
   );
 };
 
